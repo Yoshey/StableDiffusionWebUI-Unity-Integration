6d634fe24a005051d93676a87e3bed252dcb3348
Yoshey <34254960+Yoshey@users.noreply.github.com>
2023-05-08 02:25:59 +0200

    ScriptableSingleton

--- Assets/StableDiffusion-Integration/Scripts/StableDiffusionConfig.cs
+++ Assets/StableDiffusion-Integration/Scripts/StableDiffusionConfig.cs
@@ -1,13 +1,11 @@
 using UnityEngine;
+using UnityEditor;
 
 namespace StableDiffusion
 {
     [CreateAssetMenu(fileName = "SD Config", menuName = "Stable Diffusion/Config File", order = 1), ExecuteInEditMode]
-    public class StableDiffusionConfig : ScriptableObject
+    public class StableDiffusionConfig : ScriptableSingleton<StableDiffusionConfig>
     {
-        private static StableDiffusionConfig _instance;
-        public static StableDiffusionConfig instance { get { return _instance; } private set { _instance = value; } }
-
         [Header("Login")]
         [Tooltip("URL address, without http:// at the start and without the backslash at the end")]
         [SerializeField] public string address;
@@ -27,36 +25,5 @@ namespace StableDiffusion
         //public bool ABG_extension = false;
         public bool stable_diffusion_webui_rembg = false;
         #endregion
-
-        private void Awake()
-        {
-            if (instance == null)
-            {
-                instance = this;
-            }
-            else if (instance != this)
-            {
-                Debug.LogError("Multiple Stable Diffusion Config files detected! Only one should exist in the project files!");
-            }
-        }
-
-        private void OnValidate()
-        {
-            Awake();
-        }
-
-        private void Reset()
-        {
-            instance = null;
-            Awake();
-        }
-
-        private void OnDestroy()
-        {
-            if (_instance == this)
-            {
-                _instance = null;
-            }
-        }
     }
 }
